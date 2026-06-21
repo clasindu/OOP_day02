3081c02b04a399b3903518b2af4499776b37c92b
clasindu <lasindura00@gmail.com>
2026-06-21 22:05:43 +0530

    create a Payment class

---
+++ src/Payment.java
@@ -0,0 +1,23 @@
+public abstract class Payment {
+
+    public String paymentId;
+    public double amount;
+
+    public Payment(String paymentId ,double amount){
+        this.paymentId   = paymentId;
+        this.amount      = amount;
+
+    }
+
+    // common method
+
+    public void showPaymentInfo() {
+        System.out.println("Payment ID: " + paymentId + ", Amount : " + amount);
+
+    }
+
+    // abstract method - subclasses must implement
+    public abstract void processPayment();
+
+
+}
