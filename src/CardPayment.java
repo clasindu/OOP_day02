d06bfa6e1a79f80af0552aaaa9a7d2906e925e91
clasindu <lasindura00@gmail.com>
2026-06-21 22:27:10 +0530

    test the object

---
+++ src/CardPayment.java
@@ -0,0 +1,19 @@
+public class CardPayment extends Payment {
+
+    public String cardNumber;
+
+    public CardPayment(String paymentId , double amount , String cardNumber){
+
+        super(paymentId,amount);
+        this.cardNumber = cardNumber;
+
+
+    }
+
+    public void processPayment(){
+        System.out.println("Processing card payment of Rs. " + amount + " using card: " + cardNumber);
+
+    }
+
+
+}
