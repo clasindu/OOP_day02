9dab14db79c9da39127c03b071ff628d517045c3
clasindu <lasindura00@gmail.com>
2026-06-23 17:00:27 +0530

    test the object

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         CardPayment cp = new CardPayment("07", 25000.00,"42127336836");
         cp.processPayment();
         cp.showPaymentInfo();
+        cp.getCardHolderAddress();
 
         CardPayment cp1= new CardPayment("07", 25000.00,"42127336836");
         cp1.processPayment();
