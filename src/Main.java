c2ca0eb8ae000fb1a9498ca3416e7b7876fb49ca
clasindu <lasindura00@gmail.com>
2026-07-02 12:28:27 +0530

    test upcasting

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args){
-
-        CardPayment cp = new CardPayment("07", 25000.00,"42127336836");
+        //upcasting
+        Payment cp = new CardPayment("07", 25000.00,"42127336836");
         cp.processPayment();
         cp.showPaymentInfo();
-        cp.getCardHolderAddress();
+
 
         CardPayment cp1= new CardPayment("07", 25000.00,"42127336836");
         cp1.processPayment();
